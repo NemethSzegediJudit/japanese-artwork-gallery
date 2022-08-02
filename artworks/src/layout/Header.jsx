e6d3087727197d59ab51cfb6eb345530844ad271
NemethSzegediJudit <szegedijudit.gyp@gmail.com>
2022-08-02 23:51:53 +0200

    header structure and css

--- artworks/src/layout/Header.jsx
+++ artworks/src/layout/Header.jsx
@@ -1,17 +1,20 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 import "./Header.css";
 
 export default function Header() {
   return (
     <header>
+      <div className="container">
         <h1>thumbRules</h1>
         <nav>
-            <Link to="/">Home</Link>
-            <Link to="/favorites">Favorites</Link>
-            <Link to="/login">Login<i className="fa-solid fa-arrow-right-to-bracket"></i></Link>
-            
+          <Link to="/">Home</Link>
+          <Link to="/favorites">Favorites</Link>
+          <Link to="/login">
+            Log in<i className="fa-solid fa-arrow-right-to-bracket"></i>
+          </Link>
         </nav>
+      </div>
     </header>
-  )
+  );
 }
