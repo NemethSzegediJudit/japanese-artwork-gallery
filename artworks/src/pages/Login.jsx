a2bb81dd5ea143893255e85a3db50db0eb642a1e
NemethSzegediJudit <szegedijudit.gyp@gmail.com>
2022-08-02 23:51:32 +0200

    login structure and css

--- artworks/src/pages/Login.jsx
+++ artworks/src/pages/Login.jsx
@@ -1,11 +1,29 @@
 import React from "react";
 import Page from "../layout/Page";
-import "./Login.css"
+import "./Login.css";
 
 export default function Login() {
   return (
     <Page>
-      <div>Login</div>
+      <div className="login-wrapper">
+        <form>
+          <label>
+            <p>Username</p>
+            <input type="text" />
+          </label>
+          <label>
+            <p>Password</p>
+            <input type="password" />
+          </label>
+          <div>
+            <button type="submit" className="login">
+              Log in
+            </button>
+          </div>
+        </form>
+        <button className="new-account">Create new account</button>
+      </div>
+      
     </Page>
   );
 }
