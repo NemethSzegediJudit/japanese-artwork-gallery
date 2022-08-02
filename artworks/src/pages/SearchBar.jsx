b4abfec28fd51a186ec987dc8c92ba9859f71048
NemethSzegediJudit <szegedijudit.gyp@gmail.com>
2022-08-02 23:51:18 +0200

    searchbar structure and css

--- artworks/src/pages/SearchBar.jsx
+++ artworks/src/pages/SearchBar.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React from "react";
+import "./SearchBar.css";
 
 export default function SearchBar() {
   return (
-    <div>SearchBar<i className="fa-solid fa-magnifying-glass"></i></div>
-    
-  )
+    <div className="search-bar">
+      <input type="text" placeholder="Search" />
+      <i className="fa-solid fa-magnifying-glass"></i>
+    </div>
+  );
 }
