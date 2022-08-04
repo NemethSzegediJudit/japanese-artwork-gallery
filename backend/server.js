aee3c7c6a371e16f4bb113e730ed48a03579f94c
kvcsPat <smth.patrik@gmail.com>
2022-08-04 20:53:48 +0200

    post and delete request done

--- backend/server.js
+++ backend/server.js
@@ -8,27 +8,73 @@ app.use(express.json());
 app.use(cors());
 
 //----------Endpoint for favorites----------
-app.get("/favorites", (req, res) => {
+app.get("/favorites/:userId", (req, res) => {
+  let { userId } = req.params;
+
   fs.readFile("favorites.json", "utf8", (err, data) => {
     const favorites = JSON.parse(data);
 
     if (err) throw err;
 
-    res.send(favorites);
+    res.send(favorites[userId] || []);
   });
 });
 
 //----------Endpoint for POST favorites----------
-app.post("/add-to-favorites", (req, res) => {
+app.post("/favorites", (req, res) => {
+  let userId = req.body.userId;
+  let artworkId = req.body.artworkId;
+
   fs.readFile("favorites.json", "utf8", (err, data) => {
     const favorites = JSON.parse(data);
 
-    favorites.push(req.body);
+    if (favorites[userId] === undefined) {
+      favorites[userId] = [];
+    }
 
-    fs.writeFile("favorites.json", JSON.stringify(favorites), (err) => {
-      if (err) throw err;
-      console.log("Artwork is added to favorites");
-    });
+    if (!favorites[userId].includes(artworkId)) {
+      favorites[userId].push(artworkId);
+    }
+
+    fs.writeFile(
+      "favorites.json",
+      JSON.stringify(favorites, null, 4),
+      (err) => {
+        if (err) throw err;
+        console.log("Artwork is added to favorites");
+        res.send();
+      }
+    );
+  });
+});
+
+//----------Endpoint for delete from favorites----------
+app.delete("/favorites", (req, res) => {
+  const userId = req.body.userId;
+  const artworkIdToDelete = req.body.artworkId;
+
+  fs.readFile("favorites.json", "utf8", (err, data) => {
+    const favorites = JSON.parse(data);
+
+    if (favorites[userId] === undefined) {
+      res.send();
+      return;
+    }
+
+    favorites[userId] = favorites[userId].filter(
+      (artworkId) => artworkId !== artworkIdToDelete
+    );
+    /* a filter egy true/false-t ad vissza, ezért a filtert így használjuk, ahogy fentebbb*/
+
+    fs.writeFile(
+      "favorites.json",
+      JSON.stringify(favorites, null, 4),
+      (err) => {
+        if (err) throw err;
+        console.log("Artwork is deleted from favorites");
+        res.send();
+      }
+    );
   });
 });
 
