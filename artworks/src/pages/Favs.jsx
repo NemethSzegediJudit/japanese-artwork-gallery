2ffbb0bb7cb8583c6f885d1b746fa3e7afdd4bee
kvcsPat <smth.patrik@gmail.com>
2022-08-04 12:05:17 +0200

    favs to login in jsx file

--- artworks/src/pages/Favs.jsx
+++ artworks/src/pages/Favs.jsx
@@ -1,18 +1,28 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Page from "../layout/Page";
 import SearchBar from "./SearchBar";
 
 import "./Favs.css";
 
 export default function Favs() {
+  const navigate = useNavigate();
+
+  function handleClick() {
+    navigate("/login");
+  }
+
   return (
     <Page>
       <SearchBar />
       <Link to="/">
         <i className="fa-solid fa-arrow-left-long"></i>
       </Link>
-      <div>Favorites</div>
+      <div className="loginWrapper">
+        <button className="loginInFavs" onClick={handleClick}>
+          Please log in
+        </button>
+      </div>
     </Page>
   );
 }
